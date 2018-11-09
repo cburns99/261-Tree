b192454ff24d0d7694d96aab9c7383cca8dcf48c
Cameron Burns <burnscam@oregonstate.edu>
2018-11-09 11:16:39 -0800

    added traversal

--- bst.py
+++ bst.py
@@ -10,17 +10,17 @@ class BinarySearchTree:
 	def insert(self, insertee):
    		if self.value is None: 
    			self.value = insertee 
-   		else: 
-   			if self.value < insertee.value: 
-   				if self.right is None: 
-   					self.right = insertee 
-   				else: 
-   					insert(self.right, insertee) 
+ 
+   		elif insertee < self.value:
+   			if self.left is None:
+   				self.left = BinarySearchTree(insertee)
    			else:
-   				if self.left is None:
-   					self.left = insertee
-   				else: 
-   					insert(self.left, insertee) 
+   				self.left.insert(insertee)
+   		else:
+   			if self.right is None:
+   				self.right = BinarySearchTree(insertee)
+   			else: 
+   				self.right.insert(insertee)
    	
 	def find(self, item):  
 		if self.value is None:
@@ -32,14 +32,6 @@ class BinarySearchTree:
 		elif self.value < value:
 			return self.left.find(item)
 
-	def pre_order(self):
-		pre_order_list = [self.value]
-		if self.left:
-			pre_order_list.extend(self.left.pre_order())
-		if self.right:
-			pre_order_list.extend(self.right.pre_order())
-		return pre_order_list
-
 	def post_order(self):
 		post_order_list = []
 		if self.left:
@@ -47,13 +39,21 @@ class BinarySearchTree:
 		if self.right:
 			post_order_list.extend(self.right.post_order())
 		post_order_list.append(self.value)
-		return [post_order_list]
+		return post_order_list
 
 	def in_order(self):
 		in_order_list = []
 		if self.left:
 			in_order_list.extend(self.left.in_order())
 		if self.right:
-			in_order_list.extend(self.right.inorder())
+			in_order_list.extend(self.right.in_order())
 		in_order_list.append(self.value)
 		return in_order_list
+
+	def pre_order(self):
+		pre_order_list = [self.value]
+		if self.left:
+			pre_order_list.extend(self.left.pre_order())
+		if self.right:
+			pre_order_list.extend(self.right.pre_order())
+		return pre_order_list
--- test_bst.py
+++ test_bst.py
@@ -9,6 +9,12 @@ class TestBinarySearchTree(unittest.TestCase):
 			BinarySearchTree()
 		except NameError:
 			self.fail("could not instantiate")
+
+	def test_new_insert_method(self):
+		insertee = 10
+		bst = BinarySearchTree()
+		bst.insert(insertee)
+		self.assertEqual(insertee, bst.value)
 			
 	def test_instantiation_with_value(self):
 		fake_vlue = "fake"
@@ -16,43 +22,54 @@ class TestBinarySearchTree(unittest.TestCase):
 		self.assertEqual(fake_vlue, bst.value)
 
 	def test_insert_smaller(self):
-		bst = BinarySearchTree(10)
-		insertee = BinarySearchTree(1)
+		bst = BinarySearchTree()
+		insertee = 10
+		insertee2 = 1
 		bst.insert(insertee)
-		self.assertEqual(insertee, bst.left)
+		bst.insert(insertee2)
+		self.assertEqual(insertee2, bst.left.value)
 
 	def test_insert_larger(self):
-		bst = BinarySearchTree(10)
-		insertee = BinarySearchTree(20)
+		bst = BinarySearchTree()
+		insertee = 10
+		insertee2 = 20
 		bst.insert(insertee)
-		self.assertEqual(insertee,bst.right)
-   
+		bst.insert(insertee2)
+		self.assertEqual(insertee2,bst.right.value)
+
+
 	def test_find(self):
 		item = 5
 		bst = BinarySearchTree(item)
 		return_item = bst.find(item)
 		self.assertEqual(item, return_item)
 
-	def test_preorder(self):
-		bst = BinarySearchTree()
-		bst.insert(1)
-		test_list = [1]
-		test = bst.pre_order()
-		self.assertEqual(test, test_list)
-'''
 	def test_postorder(self):
 		bst = BinarySearchTree()
-		bst.insert()
-		test_list = []
+		bst.insert(1)
+		bst.insert(2)
+		bst.insert(3)
+		test_list = [3,2,1]
 		test = bst.post_order()
 		self.assertEqual(test, test_list)
 
 	def test_inorder(self):
 		bst = BinarySearchTree()
 		bst.insert(1)
-		test_list = [1]
+		bst.insert(2)
+		bst.insert(3)
+		test_list = [3,2,1]
 		test = bst.in_order()
 		self.assertEqual(test, test_list)
-'''
+	
+	def test_preorder(self):
+		bst = BinarySearchTree()
+		bst.insert(1)
+		bst.insert(2)
+		bst.insert(3)
+		test_list = [1, 2, 3]
+		test = bst.pre_order()
+		self.assertEqual(test_list, test)
+
 if __name__=='__main__':
 	unittest.main()
